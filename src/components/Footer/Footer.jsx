2c9a475ed49b8d2ff55317e7a7b6143cd0d340ed
Ilter Zekerie <103379086+ilto86@users.noreply.github.com>
2023-11-17 21:47:19 +0200

    Added components(Header, Slider,  Info, About, OurServices, Testimonials, Products, Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+    return (
+    <>
+        {/* footer section */}
+        <section className="footer_section">
+        <div className="container">
+            <p>
+            © <span>2023</span>All Rights Reserved By
+            <a href="https://html.design/">Free Html Templates</a>
+            </p>
+        </div>
+        </section>
+        {/* footer section */}
+    </>
+    )
+}
---
+++ src/components/HeroArea/HeroArea.jsx
@@ -0,0 +1,13 @@
+import Header from '../Header/Header.jsx'
+import Slider from '../Slider/Slider.jsx'
+
+export default function HeroArea() {
+    return (
+    <>
+        <div className="hero_area">
+        <Header className="navbar navbar_expand_lg" />
+        <Slider />
+        </div>
+    </>
+    )
+}
